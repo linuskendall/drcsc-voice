a4a2cdcc1f97f5dff9724f9f5a6b5e3d87fb013c
Linus Kendall <me@linuskendall.com>
2014-06-24 14:31:16 +0530

    Added user registration to the call flow
    Users can now register, as per issue #1 as well as handling
    for non registered users is there as per issue #5

--- messages.sql
+++ messages.sql
@@ -5,13 +5,14 @@ DROP TABLE IF EXISTS `messages`;
 
 CREATE TABLE `messages` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `user_id` mediumint(9) NOT NULL,
   `callid` varchar(255) DEFAULT NULL,
   `callerid` varchar(255) DEFAULT NULL,
   `filename` varchar(255) DEFAULT NULL,
   `replyto` mediumint(9) DEFAULT NULL,
   `archived` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
 
 --
 -- Used to store user meta data
@@ -19,9 +20,9 @@ CREATE TABLE `messages` (
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
-  `ìd` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `callid` varchar(255) NOT NULL,
   `location` varchar(255) DEFAULT NULL,
   `language` varchar(3) DEFAULT 'bn',
-   PRIMARY KEY (`callid`)
-)
+   PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
